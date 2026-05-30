3dad0f8c68acbf0e31262b003f950d7225be22f9
hamidmughal005 <hamidmughal4561@gmail.com>
2026-05-30 16:05:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pickle
-import os
 import numpy as np
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-model = pickle.load(open(os.path.join(BASE_DIR, "mobile_price_model.pkl"), "rb"))
-scaler = pickle.load(open(os.path.join(BASE_DIR, "scaler.pkl"), "rb"))
+model = pickle.load(open("mobile_price_model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 st.title("📱 Mobile Price Prediction App")
 
-ram = st.number_input("RAM")
-battery = st.number_input("Battery")
+battery = st.number_input("Battery (mAh)", 1000, 8000, 4000)
+ram = st.number_input("RAM (GB)", 1, 32, 4)
+camera = st.number_input("Camera (MP)", 5, 200, 48)
 
-if st.button("Predict"):
-    data = np.array([[ram, battery]])
-    prediction = model.predict(data)
+if st.button("Predict Price"):
+    input_data = np.array([[battery, ram, camera]])
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+
     st.success(f"Predicted Price: {prediction[0]}")
